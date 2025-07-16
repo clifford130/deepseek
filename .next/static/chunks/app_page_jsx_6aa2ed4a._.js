fe0c38e29cd37d3a70205077774729b1eaaf28bb
clifford130 <cliffordonchomba483@gmail.com>
2025-07-16 12:21:46 +0300

    Ignore .next build artifacts

--- .next/static/chunks/app_page_jsx_6aa2ed4a._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_f119b041._.js",
-  "static/chunks/_d13351a9._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_page_jsx_de276ff4._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_83157bc3._.js",
-  "static/chunks/_4da4fa07._.js"
-],
-    source: "dynamic"
-});
